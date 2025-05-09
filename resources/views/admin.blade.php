8f21e48b241c9aaf233ef87cb24c8cf028cdd3e7
Enio Aiello <aielloenio@icloud.com>
2025-05-09 20:03:22 +0200

    Mise à jour de la page d'administration

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,7 +9,15 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="font-semibold text-xl">{{ __("Bienvenue :name", ['name' => $user->name]) }}</h2>
+                        <h2 class="font-semibold text-xl">{{ __("Bienvenue ", ['name' => $user->name]) }}</h2>
+                        @if ($device->isNotEmpty())
+                            <p>{{ __("Vous avez ", ['count' => $deviceCount]) }} {{ __(" appareils enregistrés.") }}</p>
+                        @else
+                            <p>{{ __("Aucun appareil enregistré !") }}</p>
+                        @endif
+                        @if ($device->isNotEmpty())
+                            <p>{{ __("Vous avez ", ['repair' => $repairCount]) }} {{ __(" réparations.") }}</p>
+                        @endif
                     </div>
                 </div>
             </div>
