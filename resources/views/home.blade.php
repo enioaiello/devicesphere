066a34431b1bd09be500efeec6bbbd0e56e289c8
Enio Aiello <aielloenio@icloud.com>
2025-05-26 19:39:52 +0200

    Amélioration de la mise en page de la page d'accueil pour les utilisateurs administrateurs : réorganisation des sections et ajustement des styles.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,52 +6,40 @@
     </x-slot>
 
     @if ($user->role === 'admin')
-        <div class="py-3">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="font-semibold text-xl">{{ __("Appareils") }}</h2>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <h2 class="font-semibold text-xl">{{ __("Appareils") }}</h2>
+                    <ul>
                         @foreach ($randomDevices as $device)
                             <li>{{ $device->name }}</li>
                         @endforeach
-                        <a href="{{ route("devices") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
-                            Tous les appareils
-                        </a>
-                    </div>
+                    </ul>
+                    <a href="{{ route("devices") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
+                        Tous les appareils
+                    </a>
                 </div>
-            </div>
-        </div>
-        <div class="py-3">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="font-semibold text-xl">{{ __("Réparations") }}</h2>
-                        <a href="{{ route("repairs") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
-                            Toutes les réparations
-                        </a>
-                    </div>
+
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <h2 class="font-semibold text-xl">{{ __("Réparations") }}</h2>
+                    <a href="{{ route("repairs") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
+                        Toutes les réparations
+                    </a>
                 </div>
-            </div>
-        </div>
-        <div class="py-3">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="font-semibold text-xl">{{ __("Prêts") }}</h2>
-                        <a href="{{ route("loans") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
-                            Tous les prêts
-                        </a>
-                    </div>
+
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <h2 class="font-semibold text-xl">{{ __("Prêts") }}</h2>
+                    <a href="{{ route("loans") }}" class="block text-right text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
+                        Tous les prêts
+                    </a>
                 </div>
             </div>
         </div>
     @else
-        <div class="py-3">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="font-semibold text-xl">{{ __("Mes prêts") }}</h2>
-                    </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <h2 class="font-semibold text-xl">{{ __("Mes prêts") }}</h2>
                 </div>
             </div>
         </div>
