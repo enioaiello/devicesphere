21e457819e9d6e69ff978e61c1e01022cf87fd40
Enio Aiello <aielloenio@icloud.com>
2025-07-01 19:35:53 +0200

    Mise à jour de la liste des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            // Composants matériels
+            ProcessorSeeder::class,
+            MemorySeeder::class,
+            GraphicsCardSeeder::class,
+            StorageSeeder::class,
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            // Appareils
+            DeviceSeeder::class,
         ]);
     }
 }
